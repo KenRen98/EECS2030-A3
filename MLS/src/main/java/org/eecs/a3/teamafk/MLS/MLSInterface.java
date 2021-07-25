50eaf5b53846592a73b8e8d4e52f60d1e3735ede
Dexuan Ren <81537377+KenRen98@users.noreply.github.com>
2021-07-25 03:33:54 -0400

    javadoc

--- MLS/src/main/java/org/eecs/a3/teamafk/MLS/MLSInterface.java
+++ MLS/src/main/java/org/eecs/a3/teamafk/MLS/MLSInterface.java
@@ -1,5 +1,10 @@
 package org.eecs.a3.teamafk.MLS;
 
+/**
+ * A Interface for display and further functions
+ * @author Ken Ren
+ * @version 1.0
+ */
 public interface MLSInterface {
     String Display();
 }
